58a55500700b7631198197cd4e3015dbdddd732f
V.Sazonov <VitaliySazonov20@gmail.com>
2024-12-25 14:25:19 +0400

    RequestParam annotation

--- src/main/java/com/sazonov/spring/mvc/MyController.java
+++ src/main/java/com/sazonov/spring/mvc/MyController.java
@@ -5,6 +5,7 @@ import org.springframework.stereotype.Component;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.multipart.support.AbstractMultipartHttpServletRequest;
 
 
@@ -25,15 +26,22 @@ public class MyController {
 //    public String showEmpDetails(){
 //        return "show-emp-details-view";
 //    }
-    @RequestMapping("/showDetails")
-    public String showEmpDetails(HttpServletRequest request, Model model) {
+//    @RequestMapping("/showDetails")
+//    public String showEmpDetails(HttpServletRequest request, Model model) {
+//
+//        String empName = request.getParameter("employeeName");
+//        empName = "Mr. "+empName;
+//        model.addAttribute("nameAttribute",empName);
+//        model.addAttribute("description","- spring course Student");
 
-        String empName = request.getParameter("employeeName");
-        empName = "Mr. "+empName;
+//        return "show-emp-details-view";
+//    }
+    @RequestMapping("/showDetails")
+    public String showEmpDetails(@RequestParam("employeeName") String empName,
+                                 Model model) {
+        empName = "Mr. "+empName+ "!!";
         model.addAttribute("nameAttribute",empName);
         model.addAttribute("description","- spring course Student");
-
-
 
         return "show-emp-details-view";
     }
